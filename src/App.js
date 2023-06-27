1c8865aff3c7185c45b4eddd9bc5027655dbc54a
Ulises <ulises.argueta95@gmail.com>
2023-06-27 14:41:32 -0500

    Added NavBar css to components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import "./components/NavBar.css"
 
 function App() {
   return (
     <div className="App">
       <Router>
-
+        <NavBar />
         <Routes>
           <Route path="/"/>
           <Route path="/cart"/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar () {
-    return <div>NavBar</div>
+    return (
+     <div className="navbar">
+        <div className="links">
+            <Link to="/"> Shop </Link>
+            <Link to="/cart"></Link>
+        </div>
+     </div>
+    )
 };
 
 export default NavBar;
