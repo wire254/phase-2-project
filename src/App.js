546ef4783f28ce7ee648c91c79d03739d6729734
Ulises <ulises.argueta95@gmail.com>
2023-06-27 11:29:40 -0500

    NavBar component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
