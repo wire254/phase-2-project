67adbf8ef1614bfd7eee392be2471493b8c19ee4
Ulises <ulises.argueta95@gmail.com>
2023-07-08 12:25:04 -0500

    fetch data from deployed json-server

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 
-function Product (props) {
-    const {id, name, artist, image, info} = props.data;
-    return(
+function Product () {
+     return(
         <div className="product">
-            <img src={image}/>
-            <div className="info">
-                <p>
-                    <b>{name}</b>
-                </p>
-                <p>
-                    <b>{artist}</b>
-                </p>
-            </div>
-            <button className="addToCartBttn"> Add To Cart</button>
+            
         </div>
     );
 };
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { PRODUCTS } from "../../products";
-import Product from "./product";
+import React, { useState, useEffect} from "react";
 import "./shop.css";
 
 function Shop() {
+
+    const apiGet = useEffect(() => {
+        fetch("https://phase-2-project-json-server-n38l.onrender.com/PRODUCTS/1")
+        .then((res) => res.json())
+        .then(productData => console.log(productData))
+    })
     return (
         <div className="shop">
             <div className="shopTitle">
                 <h1> Chicago Institute Shop </h1>
             </div>
             <div className="products">
-                {PRODUCTS.map((product) => (
-                    <Product data={product}/>
-                ))}
+
+
             </div>
         </div>
     )
